fd68192fd264776d9e33de1ba7fddc37310b45a2
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-07-22 19:10:14 -0300

    fix: change query from LIKE to ILIKE

--- src/games/gamesController.js
+++ src/games/gamesController.js
@@ -4,7 +4,7 @@ async function getGames(req, res) {
   let dbQuery = 'SELECT games.id AS id, games.name, image, "stockTotal", "categoryId", "pricePerDay", categories.name AS "categoryName" FROM games  INNER JOIN categories ON games."categoryId" = categories.id';
   const {name} = req.query;
   if(name){
-    dbQuery =  dbQuery + ` WHERE games.name LIKE '%${name}%'`;
+    dbQuery =  dbQuery + ` WHERE games.name ILIKE '%${name}%'`;
   }
   const {rows: game} = await connection.query(dbQuery);
   res.status(200).send(game);
