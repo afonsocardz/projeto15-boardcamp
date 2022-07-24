17e6b94484d12ac40d00ef64a6ffd36c9f2cd3cd
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-07-24 16:49:51 -0300

    feat: order by column and filter status and date

--- src/utils/offsetQueryMiddleware.js
+++ src/utils/offsetQueryMiddleware.js
@@ -1,16 +1,22 @@
-export default function offsetQuery(req,res, next){
+export default function offsetQuery(req, res, next) {
   let dbQuery = res.locals.dbQuery;
   let values = res.locals.values;
   if (!values) values = [];
-  const {offset, limit}  = req.query;
-  if(offset){
+  const { offset, limit, order, desc } = req.query;
+  
+  if (offset) {
     dbQuery += ' OFFSET $' + (values.length + 1);
     values.push(offset);
-    console.log(dbQuery);
   }
-  if(limit){
+  if (limit) {
     dbQuery += ' LIMIT $' + (values.length + 1);
     values.push(limit);
+  }
+  if (order) {
+    dbQuery += ` ORDER BY "${order}"`;
+    if (desc) {
+      dbQuery += ' DESC';
+    }
   }
   res.locals.dbQuery = dbQuery;
   res.locals.values = values;
